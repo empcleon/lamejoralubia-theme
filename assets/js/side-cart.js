36e65d29b8ef752fd1e1272d088e367fc26626ec
Emma <emma@lamejoralubia.com>
2026-04-01 12:45:44 +0200

    refactor: side cart to WooCommerce native - no more custom AJAX

--- assets/js/side-cart.js
+++ assets/js/side-cart.js
@@ -1,7 +1,5 @@
 /**
- * Side Cart - JavaScript del carrito lateral
- * 
- * Carrito lateral estilo Amazon/Zara
+ * Side Cart - JavaScript (WooCommerce Native)
  * 
  * @package La_Mejor_Alubia
  */
@@ -9,248 +7,35 @@
 jQuery(function($) {
     'use strict';
 
-    var SideCart = {
-        init: function() {
-            this.createCartElements();
-            this.bindEvents();
-            this.refreshOnAjax();
-            this.updateInitialCount();
-        },
+    // Abrir carrito al añadir producto
+    $(document.body).on('added_to_cart', function(event, fragments, cart_hash, $button) {
+        // Refresca fragments
+        $(document.body).trigger('wc_fragment_refresh');
+        
+        // Abre carrito
+        $('body').addClass('cart-open');
+    });
 
-        createCartElements: function() {
-            var cartHtml = `
-                <div class="cart-panel-overlay" id="cart-overlay"></div>
-                <div class="cart-panel" id="side-cart">
-                    <div class="cart-panel-header">
-                        <h3>Tu Carrito</h3>
-                        <button class="cart-panel-close" aria-label="Cerrar carrito">&times;</button>
-                    </div>
-                    <div class="cart-panel-content" id="cart-panel-content">
-                        ${this.getCartContent()}
-                    </div>
-                    <div class="cart-panel-footer" id="cart-panel-footer">
-                        ${this.getCartFooter()}
-                    </div>
-                </div>
-                <div class="floating-cart-icon" id="floating-cart">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/>
-                    </svg>
-                    <span class="cart-count">0</span>
-                </div>
-            `;
-            $('body').append(cartHtml);
-        },
+    // Cerrar carrito al hacer click en X
+    $('.close-cart').on('click', function() {
+        $('body').removeClass('cart-open');
+    });
 
-        getCartContent: function() {
-            var self = this;
-            var content = '';
-            
-            $.ajax({
-                url: '/wp-admin/admin-ajax.php',
-                type: 'POST',
-                data: {
-                    action: 'get_cart_content_html'
-                },
-                async: false,
-                success: function(response) {
-                    if (response && response.success && response.data) {
-                        if (response.data.is_empty) {
-                            content = '<div class="empty-cart"><p>Tu carrito está vacío</p><a href="' + window.location.origin + '/shop/" class="button">Ver productos</a></div>';
-                        } else {
-                            content = response.data.html;
-                        }
-                    }
-                }
-            });
-            
-            if (!content) {
-                var $miniCart = $('.widget_shopping_cart_content');
-                if ($miniCart.length && $miniCart.find('.woocommerce-mini-cart__empty-message').length === 0) {
-                    content = $miniCart.html();
-                } else {
-                    content = '<div class="empty-cart"><p>Tu carrito está vacío</p><a href="' + window.location.origin + '/tienda/" class="button">Ver productos</a></div>';
-                }
-            }
-            
-            return content;
-        },
+    // Cerrar carrito al hacer click en overlay
+    $('.cart-overlay').on('click', function() {
+        $('body').removeClass('cart-open');
+    });
 
-        getCartFooter: function() {
-            var self = this;
-            var subtotal = '0,00 €';
-            var cartUrl = window.location.origin + '/cart/';
-            var checkoutUrl = window.location.origin + '/checkout/';
-            
-            $.ajax({
-                url: '/wp-admin/admin-ajax.php',
-                type: 'POST',
-                data: {
-                    action: 'get_cart_subtotal'
-                },
-                async: false,
-                success: function(response) {
-                    if (response && response.success && response.data && response.data.subtotal) {
-                        subtotal = response.data.subtotal;
-                    }
-                }
-            });
-
-            return `
-                <div class="cart-subtotal">
-                    <span>Subtotal:</span>
-                    <span class="amount">${subtotal}</span>
-                </div>
-                <a href="${cartUrl}" class="wc-forward">Ver Carrito</a>
-                <a href="${checkoutUrl}" class="wc-forward" style="margin-top: 10px; background: #228B22;">Finalizar Compra</a>
-            `;
-        },
-
-        bindEvents: function() {
-            var self = this;
-
-            $('#floating-cart').on('click', function(e) {
-                e.preventDefault();
-                self.openCart();
-            });
-
-            $('.cart-panel-close, #cart-overlay').on('click', function(e) {
-                e.preventDefault();
-                self.closeCart();
-            });
-
-            $(document).keyup(function(e) {
-                if (e.keyCode === 27) {
-                    self.closeCart();
-                }
-            });
-
-            // ABRIR CARRITO AL AÑADIR PRODUCTO - sin condiciones
-            $('body').on('click', '.add_to_cart_button', function(e) {
-                setTimeout(function() {
-                    self.openCart();
-                    self.refreshCartData();
-                }, 500);
-            });
-
-            // WooCommerce AJAX add to cart
-            $(document).on('added_to_cart', function(event, fragments, cart_hash, $button) {
-                self.openCart();
-                self.refreshCartData();
-            });
-        },
-
-        openCart: function() {
-            $('#side-cart, #cart-overlay').addClass('open');
-            $('body').addClass('cart-panel-open');
-        },
-
-        closeCart: function() {
-            $('#side-cart, #cart-overlay').removeClass('open');
-            $('body').removeClass('cart-panel-open');
-        },
-
-        refreshOnAjax: function() {
-            var self = this;
-
-            // WooCommerce fragments refresh
-            $(document).on('wc_fragment_refreshed wc_cart_updated', function() {
-                self.refreshCartData();
-            });
-        },
-
-        refreshCartData: function() {
-            var self = this;
-            
-            $.ajax({
-                url: woocommerce_params.ajax_url,
-                type: 'POST',
-                data: {
-                    action: 'woocommerce_get_refreshed_fragments'
-                },
-                success: function(response) {
-                    if (response && response.fragments) {
-                        self.updateCartCount(response.fragments);
-                        self.updateCartContent(response.fragments);
-                    }
-                },
-                error: function() {
-                    self.updateCartCountFromDOM();
-                }
-            });
-        },
-
-        updateCartCount: function(fragments) {
-            var $count = $('.floating-cart-icon .cart-count');
-            
-            if (fragments && fragments['.ast-cart-menu-quantity']) {
-                $count.text(fragments['.ast-cart-menu-quantity'].text());
-            } else if (fragments && fragments['.cart-count']) {
-                $count.text(fragments['.cart-count'].text());
-            } else {
-                this.updateCartCountFromDOM();
-            }
-        },
-
-        updateCartCountFromDOM: function() {
-            var $count = $('.floating-cart-icon .cart-count');
-            var $miniCart = $('.widget_shopping_cart_content');
-            
-            if ($miniCart.length) {
-                var $quantity = $miniCart.find('.quantity');
-                if ($quantity.length) {
-                    var text = $quantity.first().text();
-                    var match = text.match(/(\d+)/);
-                    if (match) {
-                        $count.text(match[1]);
-                        return;
-                    }
-                }
-                var $countEl = $miniCart.find('.count');
-                if ($countEl.length) {
-                    $count.text($countEl.text());
-                }
-            }
-        },
-
-        updateCartContent: function(fragments) {
-            var $panelContent = $('#cart-panel-content');
-            var $panelFooter = $('#cart-panel-footer');
-            
-            if (fragments && fragments['.widget_shopping_cart_content']) {
-                var $temp = $('<div>').html(fragments['.widget_shopping_cart_content']);
-                if ($temp.find('.woocommerce-mini-cart__empty-message').length === 0) {
-                    $panelContent.html($temp.html());
-                }
-            }
-            
-            this.updateCartCountFromDOM();
-        },
-
-        updateInitialCount: function() {
-            var self = this;
-            
-            $.ajax({
-                url: '/wp-admin/admin-ajax.php',
-                type: 'POST',
-                data: {
-                    action: 'get_cart_count'
-                },
-                success: function(response) {
-                    if (response && response.count !== undefined) {
-                        $('.floating-cart-icon .cart-count').text(response.count);
-                    } else {
-                        self.updateCartCountFromDOM();
-                    }
-                },
-                error: function() {
-                    self.updateCartCountFromDOM();
-                }
-            });
+    // Cerrar con ESC
+    $(document).keyup(function(e) {
+        if (e.keyCode === 27) {
+            $('body').removeClass('cart-open');
         }
-    };
+    });
 
-    $(document).ready(function() {
-        SideCart.init();
+    // Abrir carrito al hacer click en icono flotante (si existe)
+    $(document).on('click', '.floating-cart-icon, .ast-icon-shopping-cart', function(e) {
+        e.preventDefault();
+        $('body').addClass('cart-open');
     });
 });
--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -3,6 +3,8 @@
  * WooCommerce Tweaks
  * 
  * Customizaciones de WooCommerce para La Mejor Alubia
+ * 
+ * @package La_Mejor_Alubia
  */
 
 // Limitar provincias disponibles (Canarias)
@@ -15,78 +17,41 @@ function ejr_limita_envios( $provincias ) {
 }
 add_filter( 'woocommerce_states', 'ejr_limita_envios' );
 
-// Forzar actualizacion de fragmentos AJAX del carrito
+// WooCommerce fragments - NATIVO
 add_filter( 'woocommerce_add_to_cart_fragments', function( $fragments ) {
-    ob_start();
-    ?>
-    <span class="cart-count">
-        <?php echo WC()->cart->get_cart_contents_count(); ?>
-    </span>
-    <?php
-    $fragments['.floating-cart-icon .cart-count'] = ob_get_clean();
-    
-    ob_start();
-    ?>
-    <span class="ast-cart-menu-quantity">
-        <?php echo WC()->cart->get_cart_contents_count(); ?>
-    </span>
-    <?php
-    $fragments['.ast-cart-menu-quantity'] = ob_get_clean();
-    
-    return $fragments;
-});
-
-// AJAX endpoint para obtener cantidad del carrito
-add_action( 'wp_ajax_get_cart_count', 'get_cart_count_ajax' );
-add_action( 'wp_ajax_nopriv_get_cart_count', 'get_cart_count_ajax' );
-
-function get_cart_count_ajax() {
-    $count = WC()->cart->get_cart_contents_count();
-    wp_send_json( array( 'count' => $count ) );
-}
-
-// AJAX endpoint para obtener contenido del carrito
-add_action( 'wp_ajax_get_cart_content_html', 'get_cart_content_html_ajax' );
-add_action( 'wp_ajax_nopriv_get_cart_content_html', 'get_cart_content_html_ajax' );
-
-function get_cart_content_html_ajax() {
-    if ( WC()->cart->is_empty() ) {
-        wp_send_json( array( 
-            'success' => true, 
-            'data' => array( 
-                'is_empty' => true,
-                'html' => ''
-            ) 
-        ) );
-    }
-    
     ob_start();
     woocommerce_mini_cart();
     $mini_cart = ob_get_clean();
     
-    wp_send_json( array( 
-        'success' => true, 
-        'data' => array( 
-            'is_empty' => false,
-            'html' => $mini_cart
-        ) 
-    ) );
-}
-
-// AJAX endpoint para obtener subtotal del carrito
-add_action( 'wp_ajax_get_cart_subtotal', 'get_cart_subtotal_ajax' );
-add_action( 'wp_ajax_nopriv_get_cart_subtotal', 'get_cart_subtotal_ajax' );
-
-function get_cart_subtotal_ajax() {
-    $subtotal = WC()->cart->get_cart_subtotal();
-    wp_send_json( array( 
-        'success' => true, 
-        'data' => array( 'subtotal' => $subtotal ) 
-    ) );
-}
+    $fragments['.widget_shopping_cart_content'] = $mini_cart;
+    $fragments['.floating-cart-icon .cart-count'] = '<span class="cart-count">' . WC()->cart->get_cart_contents_count() . '</span>';
+    
+    return $fragments;
+});
 
 // Incluir WooCommerce en el theme
 function la_mejor_alubia_woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'la_mejor_alubia_woocommerce_support' );
+
+// Side Cart HTML en el footer
+add_action( 'wp_footer', function() {
+    if ( is_cart() || is_checkout() ) {
+        return;
+    }
+    ?>
+    <div class="cart-overlay" id="cart-overlay"></div>
+    <div class="side-cart" id="side-cart">
+        <div class="side-cart-inner">
+            <div class="side-cart-header">
+                <h3>Tu carrito</h3>
+                <button class="close-cart" aria-label="Cerrar carrito">&times;</button>
+            </div>
+            <div class="side-cart-content widget_shopping_cart_content">
+                <?php woocommerce_mini_cart(); ?>
+            </div>
+        </div>
+    </div>
+    <?php
+});
